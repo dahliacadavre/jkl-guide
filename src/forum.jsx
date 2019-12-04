5a331e655528652644c3a070c666f04354ddfc07
m3311 <m3311@student.jamk.fi>
2019-12-04 14:25:47 +0200

    update

--- src/forum.jsx
+++ src/forum.jsx
@@ -7,8 +7,18 @@ import './site.scss';
 export default class Forum extends React.Component {
     render() {
         return (
-            <div className="page">
-               <h1>Forum</h1> 
+            <div id="forumWrapper" className="page container-fluid">
+               <h1 className="px-3 pt-2">Forum</h1>
+               <div className="card mx-2">
+                   <div className="card-body">
+                        <div className="">
+                            <h3>Title</h3>
+                            <h4>Username</h4>
+                            <p>10.10.2010</p></div>
+                        <p></p>
+                   </div>
+
+               </div>
             </div>
         );
     }
